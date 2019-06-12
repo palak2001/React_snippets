52eeacee3623a05089ddc1b687bb34026346233e
palak2001 <palak.mittal.2908@gmail.com>
2019-06-13 01:35:21 +0530

    Very baic that is import functions & use of render

---
+++ React_render.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+ReactDOM.render(
+  <ul>
+    <li>Jai</li>
+    <li>MATA</li>
+    <li>Di</li>
+  </ul>,
+    document.getElementById("root")
+)
